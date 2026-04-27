065e32e0cd9a3c99fe5c0deda6a3359cf0eebab8
Itamar Junior <cdajuniorf@gmail.com>
2026-04-27 17:15:00 -0300

    chore: align per_page max in user index request

--- app/Http/Requests/User/IndexUserRequest.php
+++ app/Http/Requests/User/IndexUserRequest.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Requests\User;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class IndexUserRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('viewAny', User::class);
     }
 
     public function rules(): array
     {
         return [
-            'per_page' => ['integer', 'nullable', 'min:1', 'max:10'],
+            'per_page' => ['integer', 'nullable', 'min:1', 'max:50'],
         ];
     }
 }
